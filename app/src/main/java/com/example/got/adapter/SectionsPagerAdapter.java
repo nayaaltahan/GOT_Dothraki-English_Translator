9df7ef73713b5cdd7b29eb19a4929b4ac06952f6
Naya Al Tahan <naya.altahan00@gmail.com>
2019-11-16 02:04:17 +0100

    Fixed the tabs names in MainActivity, font, and background color

--- app/src/main/java/com/example/got/adapter/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/got/adapter/SectionsPagerAdapter.java
@@ -29,9 +29,9 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     public CharSequence getPageTitle(int position) {
         switch(position) {
             case 0:
-                return "Page 1";
+                return "Translate";
             case 1:
-                return "Page 2";
+                return "History";
             default:
                 return null;
         }
